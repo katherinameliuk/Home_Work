ee68e8b7c0325a2b82ffda84122dbf85e7e2c4ec
Kate <katherinameliuk1@gmail.com>
2022-08-05 12:26:14 +0200

    Add file HW1

---
+++ Kate_Meliuk_HW1.py
@@ -0,0 +1,6 @@
+# Посчитать площадь и периметр квадрата
+a = 4
+square = a ** 2
+perimetr = 4 * a
+print(f"Square is {square}, Perimetr is {perimetr}") # Если ставим в начале букву f, то это дает возможность строке принимать в себя какие-то значения
+print("Square:", square, "Perimetr:", perimetr)
