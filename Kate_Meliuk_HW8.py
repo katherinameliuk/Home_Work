9ebb0e2cc6ba2ccb38e52896a9d6e97ac8214e0d
Kate <katherinameliuk1@gmail.com>
2022-08-05 13:31:55 +0200

    Add file HW8

---
+++ Kate_Meliuk_HW8.py
@@ -0,0 +1,24 @@
+# Напишите программу с классом Car.
+# Создайте конструктор (__init__) класса Car.
+# Создайте атрибуты класса Car — color (цвет), type (тип), year (год).
+# Напишите три метода для этого класса.
+# Первый — запуск автомобиля, при его вызове выводится сообщение «Автомобиль заведен».
+# Второй — отключение автомобиля — выводит сообщение «Автомобиль заглушен».
+# Третий - магический метод str  для вывода атрибутов экземпляра в виде строки.
+class Car:
+    def __init__(self, color, type, year):
+        self.color = color
+        self.type = type
+        self.year = year
+
+    def __str__(self) -> str:
+        return f'Color: {self.color}, Type: {self.type}, Year: {self.year}'
+
+    def start(self):
+        return print("Car is started")
+    def finish(self):
+        return print("Car is finish")
+
+
+tesla = Car("white", "sedan", "2022")
+print(tesla)
