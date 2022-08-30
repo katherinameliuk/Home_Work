743ac0c93f7e6e4f22be5cfdcb7e7adbe13be66d
Kate <katherinameliuk1@gmail.com>
2022-08-30 08:46:53 +0200

    add changes HW16

--- Kate_Meliuk_HW16.py
+++ Kate_Meliuk_HW16.py
@@ -1,7 +1,9 @@
 WIN_DICTONARY = {"rock": ["scissors", "rock"], "scissors": ["paper", "scissors"], "paper": ["rock", "paper"]}
 
 def game(player_1: str, player_2: str, player_3: str):
-    if player_2 in WIN_DICTONARY[player_1]:
+    if player_1 == player_2 == player_3:
+        print("Tie")
+    elif player_2 in WIN_DICTONARY[player_1]:
         print(f"{player_1} is winner")
     elif player_2 not in WIN_DICTONARY[player_1]:
         print(f"{player_2} is winner")
@@ -9,10 +11,19 @@ def game(player_1: str, player_2: str, player_3: str):
         print(f"{player_1} is winner")
     elif player_3 not in WIN_DICTONARY[player_1]:
         print(f"{player_3} is winner")
-    else:
-        return print("Tie")
+
 
 game("scissors", "paper", "rock")
 game("rock", "scissors", "paper")
 game("paper", "rock", "scissors")
 
+game("paper", "scissors", "scissors")
+game("paper", "rock", "rock")
+game("rock", "paper", "paper")
+game("rock", "scissors", "scissors")
+game("scissors", "paper", "paper")
+game("scissors", "rock", "rock")
+
+game("scissors", "scissors", "scissors")
+game("paper", "paper", "paper")
+game("rock", "rock", "rock")
